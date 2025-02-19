f28a07f535a1abc8f967c24ac57832609abaca3c
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2025-02-19 10:50:22 +0200

    Time: 0 ms (100%), Space: 8.6 MB (81.88%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(haystack.find(needle)){
+            return  haystack.find(needle);
+        } else{
+            return false;
+        }
+    }
+};

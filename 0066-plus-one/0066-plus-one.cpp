19cce732bb8ecf2e7ec1678880461ca1d1187f75
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2025-02-22 12:30:02 +0200

    Time: 0 ms (100%), Space: 11.5 MB (34.65%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         for(int i = digits.size() - 1; i >= 0; i--){
-            if(digits[i] == 9) digits[i] = 0;
-            else{
+            if(digits[i] == 9){
+                digits[i] = 0;
+            } else{
                 digits[i]++;
                 return digits;
             }

0a42a406dbfb1c1eb18b52f2fa4b843ba9cf8dca
Chambit Oh <169043471+bitcham@users.noreply.github.com>
2025-05-29 19:23:34 +0900

    Time: 2 ms (75.22%), Space: 61.7 MB (64.14%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int minprice = Integer.MAX_VALUE;
+        int minprice = 987654321;
         int maxprofit = 0;
         for (int i = 0; i < prices.length; i++) {
             if (prices[i] < minprice) minprice = prices[i];
